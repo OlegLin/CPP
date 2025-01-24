07d1f01bfdfeab24d57be49681891484c27f9854
Job_st <gooleglee@mail.ru>
2025-01-24 13:51:31 +0300

    Saper

---
+++ Vectors/saper.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+
+int main(){
+    std::vector<std::vector<int>> pole;
+    int m; // number of string
+    int n; //number of columns
+    for (int i; i != m*n; ++i){
+        
+    }
+
+}
