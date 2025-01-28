0f7db7115a488a1df9fcd6580905bda62b561daa
Job_st <gooleglee@mail.ru>
2025-01-28 15:55:01 +0300

    Compose

--- Composite_data_types/QA.cpp
+++ Composite_data_types/QA.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <utility>
+#include <tuple>
+#include <string>
 
 
 using std::cout;
@@ -20,12 +23,35 @@ struct Point {
 int main(){
     Point point1;  // по умолчанию координаты будут нулевыми, а color никак не будет проинициализирован
     point1.color = Color::Red;
-
     Point point2 = {1.4, -2.2, -3.98, Color::Orange};
-
     point2.z -= 10;
-
     cout << point2.z << '\n';
+
+
+
+    std::pair<double, int> neww = {3.14, 5};
+    cout << neww.first << '\t';
+    cout << neww.second << '\n';
+
+    std::tuple<int, std::string, double, Point> tup = {1, "HI", 5.234, {.x = 3.12}};
+    cout << std::get<2>(tup) << '\t' ;
+    cout << std::get<1>(tup) << '\t' ;
+    cout << std::get<0>(tup) << '\t' ;
+    cout << std::get<3>(tup).x << '\n' ;
+    std::get<3>(tup).x = 23.232232323;
+    cout << std::get<3>(tup).x << '\n' ;
+
+
+
+
+
+
+    std::pair<std::string, int> p = {"hello", 42};
+    auto [word, freq] = p;  // word = "hello"; freq = 42;
+
+    cout << word << '\n';
+
+//Здесь конструкция auto [word, freq] = p вводит две новые переменные word и freq соответствующих типов и присваивает им значения из пары.
 
 
 
