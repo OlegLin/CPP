99933705ca606a2946750376baff0aedb4f7a4cc
Job_st <gooleglee@mail.ru>
2025-02-04 15:04:24 +0300

    nn

--- Functions/CommonPrefix.cpp
+++ Functions/CommonPrefix.cpp
@@ -5,15 +5,24 @@
 
 std::string CommonPrefix(const std::vector<std::string>& words){
     std::string answer = "";
-
-    int n = words.size(), m = words[0].size();
-    for (int i = 0; i != m; ++i){
-        for (int j = 0; j != n; ++j){
-            std::cout << words[j][i] << '\n';
-        }
-    }
+    bool flag = true;
     
+    for (int i = 0; i != words[0].size(); ++i){
+        for (int j = 0; j != words.size(); ++j){
+            if (words[i][0] != words[i][j]){answer = false;}
+        }
+        if (flag){ answer += words[i][0]; 
+        std::cout<< "YES  " << words[i][0] << "\n";  } 
+        else {std::cout << "no";}
+        
+    }
+    std::cout << answer;
+    return answer;
 }
+
+    
+    
+
 
 int main(){
     std::vector<std::string> words = {"applic", "apl", "apasdfa"};
