2cf17bd7e79a3ce699b066e71b2c53f7fd212a83
Job_st <gooleglee@mail.ru>
2025-02-10 12:51:53 +0300

    OldSchool

--- SECOND/SequentialContainers/CheackTask.cpp
+++ SECOND/SequentialContainers/CheackTask.cpp
@@ -3,6 +3,30 @@
 #include <vector>
 #include <deque> // DOUBLE ENDED !!!!
 
+
+using std::cin;
+
+
+
 int main(){
+    int n;
+    cin >> n;
+    std::deque<std::string> lists;
+
     
+    for (int i = 0; i != n; ++i){
+        std::string family;
+        std::string put;
+        cin >> family >> put;
+        if (put == "top"){
+            lists.push_front(family);
+        } else {lists.push_back(family);}
+
+    }
+    for (std::string elem : lists){
+        std::cout << elem << '\t';
+    }
+
+    std::cout << '\n';
+
 }
