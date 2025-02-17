46fea3f7209d2fa45c96272e2fce26dfa34d231a
Job_st <gooleglee@mail.ru>
2025-02-17 09:14:51 +0300

    SchoolProject

--- SECOND/SequentialContainers/RedactorShift.cpp
+++ SECOND/SequentialContainers/RedactorShift.cpp
@@ -9,20 +9,32 @@
 std::list<std::string> NewText(){
     std::list<std::string> newtext;
     std::string line;
-    while (std::getline(std::cin, line)){
+    while (std::getline(std::cin, line) && line != ""){
         newtext.push_back(line);
     }
 
     return newtext;
 }
 
-auto Up(const std::list<std::string>& text, auto& lastkursor){
+auto Up(const std::list<std::string>& text, auto lastkursor){
     if (lastkursor != text.begin()){ lastkursor--;}
     return lastkursor;
 }
 
+auto Down(const std::list<std::string>& text, auto nextkursor){
+    if (nextkursor != text.end()){ nextkursor++;}
+    return nextkursor;
+}
 
+std::vector<std::string> Buffer(std::list<std::string> text, auto kursor, int n = 1){
+    std::vector<std::string> buf;
+    for (std::size_t i = 0; i != n; ++i){
+        buf.push_back(*kursor);
+        kursor++;
+    }
+    return buf;
 
+}
 
 
 
@@ -32,11 +44,19 @@ int main(){
     auto kursor = text.begin();
     while (std::getline(std::cin, command)){
         if (command == "Up"){
-            Up(text, kursor);
-        }
+            kursor = Up(text, kursor);
+            std::cout << *kursor;
+        } else if (command == "Down"){
+            kursor = Down(text, kursor);
+            std::cout << *kursor;
+        } else if (command == "Ctrl+X"){
+
+        
+    }
     }
     
     for (std::string elem : text){
         std::cout << elem << '\n';
     }
+
 }
